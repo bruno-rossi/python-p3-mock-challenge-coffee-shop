2acc5a024f73af5d6c22298f4e58eb7c52ffa7f2
Bruno Rossi <brunogabrielrossi@gmail.com>
2024-01-18 13:46:47 -0500

    Update

--- lib/classes/coffee.py
+++ lib/classes/coffee.py
@@ -22,23 +22,15 @@ class Coffee:
         return customers
     
     def num_orders(self): # Returns the total number of times that coffee has been ordered
-        orders_per_coffee = {}
-        for order in self.orders:
-            if self.name in orders_per_coffee:
-                orders_per_coffee.update({self.name: orders_per_coffee.get(self.name) + 1})
-            else:
-                orders_per_coffee.update({self.name: 1})
-
-        print(orders_per_coffee)
-        print(orders_per_coffee[self.name])
-        return orders_per_coffee[self.name]
+        return len(self.orders)
     
     def average_price(self): # Returns the average price for a coffee based on its orders
-        
-        coffee_prices = []
-        for order in self.orders:
-            coffee_prices.append(order.price)
-        return sum(coffee_prices) / len(coffee_prices)
+        # coffee_prices = []
+        # for order in self.orders:
+        #     coffee_prices.append(order.price)
+        # return sum(coffee_prices) / len(coffee_prices)
+    
+        return sum([order.price for order in self.orders]) / self.num_orders()
 
     def __repr__(self) -> str:
         return f"<Coffee {self.name}>"
